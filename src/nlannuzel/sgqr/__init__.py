9a7d295533e78c1c85eae38da89a40193503b5c7
Nicolas Lannuzel <nlannuzel@gmail.com>
2026-03-11 15:29:37 +0800

    paynow: add pay_uen() function

--- src/nlannuzel/sgqr/__init__.py
+++ src/nlannuzel/sgqr/__init__.py
@@ -3,7 +3,7 @@ import argparse
 from nlannuzel.sgqr.encode import encode_sgqr
 from nlannuzel.sgqr.decode import decode_sgqr
 from datetime import datetime, timezone, timedelta
-from nlannuzel.sgqr.factory import paynow_phone
+from nlannuzel.sgqr.factory import paynow_phone, paynow_uen
 
 def encode():
     with open("/dev/stdin") as f:
@@ -17,16 +17,18 @@ def paynow():
     parser = argparse.ArgumentParser(
                     prog='paynow-sgqr',
                     description='encode a EMV string for PayNow')
-    parser.add_argument('-r', '--recipient', required=True, help="payee's phone number")
+    parser.add_argument('-r', '--recipient', required=True, help="payee's phone number or UEN")
+    parser.add_argument('-u', '--uen', required=False, action='store_true', help="pay to a UEN instead of a phone")
     parser.add_argument('-a', '--amount', required=True, type=float, help='amount in SGD')
-    parser.add_argument('-c', '--comment', required=False, help='comment for the recipient')
+    parser.add_argument('-c', '--comment', required=False, help='comment or bill number for the recipient')
     parser.add_argument('-t', '--time-to-live', required=False, type=int, help='how many seconds the code should be valid')
     parser.add_argument('-e', '--editable_amount', required=False, action='store_true', help='allow the amount to be edited')
     args = parser.parse_args()
 
+    func = pay_uen if args.uen else pay_phone
     print(
         yaml.dump(
-            paynow_phone(
+            func(
                 phone=args.recipient,
                 amount=args.amount,
                 comment=args.comment,
--- src/nlannuzel/sgqr/factory.py
+++ src/nlannuzel/sgqr/factory.py
@@ -35,3 +35,34 @@ def paynow_phone(recipient: str, amount: float, comment: str=None, ttl: int=None
         decoded.append(d('62', a2d([['01', comment]])))
 
     return decoded
+
+def paynow_uen(recipient: str, amount: float, comment: str=None, ttl: int=None, editable_amount: bool=False):
+    if comment is None:
+        comment = 'NA'
+
+    decoded = a2d([
+        ['00', '01'],
+        ['01', '12'],
+        ['26', a2d([
+            ['00', 'SG.PAYNOW'],
+            ['01', '2'],
+            ['02', recipient],
+            ['03', '1' if editable_amount is True else '0'],
+        ])],
+        ['52', '3000'],
+        ['53', '702'],
+        ['54', str(amount)],
+        ['58', 'SG'],
+        ['60', 'Singapore'],
+        ['62', a2d([
+            ['01', comment],
+        ])],
+    ])
+
+    if ttl is not None:
+        tz_sg = timezone(offset=timedelta(hours=8))
+        dt = datetime.now(tz_sg) + timedelta(seconds=ttl)
+        expiry = dt.strftime('%Y%m%d%H%M%S')
+        decoded[2]['value'].append(d('04', expiry))
+
+    return decoded
